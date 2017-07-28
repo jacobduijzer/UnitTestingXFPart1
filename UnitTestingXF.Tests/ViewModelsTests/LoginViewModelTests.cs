b97bb9eba3917b51c680d24486f19d01834da147
Jacob Duijzer <jacob.duijzer@redhotminute.com>
2017-07-28 09:18:24 +0200

    Changed test

--- UnitTestingXF.Tests/ViewModelsTests/LoginViewModelTests.cs
+++ UnitTestingXF.Tests/ViewModelsTests/LoginViewModelTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using NUnit.Framework;
 using UnitTestingXF.ViewModels;
 
@@ -25,56 +25,33 @@ namespace UnitTestingXF.Tests.ViewModelsTests
         public void CanLoginWithValidFormTest()
         {
             var vm = new LoginViewModel();
+            Assert.IsFalse(vm.IsFormValid);
             Assert.IsFalse(vm.LoginCommand.CanExecute(null));
 
             vm.Username = "testuser";
+            Assert.IsFalse(vm.IsFormValid);
             Assert.IsFalse(vm.LoginCommand.CanExecute(null));
 
             vm.Password = "testpassword";
-            Assert.IsTrue(vm.LoginCommand.CanExecute(null));
+            Assert.IsFalse(vm.IsFormValid);
+            Assert.IsFalse(vm.LoginCommand.CanExecute(null));
 
             vm.Username = string.Empty;
+            Assert.IsFalse(vm.IsFormValid);
             Assert.IsFalse(vm.LoginCommand.CanExecute(null));
 
             vm.Username = "     ";
+            Assert.IsFalse(vm.IsFormValid);
             Assert.IsFalse(vm.LoginCommand.CanExecute(null));
 
             vm.Password = "      ";
+            Assert.IsFalse(vm.IsFormValid);
             Assert.IsFalse(vm.LoginCommand.CanExecute(null));
-        }
-
-        [Test]
-        public void FormValidationTest()
-        {
-			var vm = new LoginViewModel();
-
-            vm.Username = "test";
-            vm.Password = "test";
-            Assert.IsFalse(vm.IsFormValid);
-
-            vm.Username = "test@test";
-            vm.Password = "test";
-            Assert.IsFalse(vm.IsFormValid);
-
-            vm.Username = "test@test.com";
-            vm.Password = "test123";
-            Assert.IsFalse(vm.IsFormValid);
-
-            vm.Username = "test@test";
-            vm.Password = "test123!";
-            Assert.IsFalse(vm.IsFormValid);
-
-            vm.Username = "test";
-            vm.Password = "test123!";
-            Assert.IsFalse(vm.IsFormValid);
-
-            vm.Username = "    ";
-            vm.Password = "    ";
-            Assert.IsFalse(vm.IsFormValid);
 
             vm.Username = "test@test.com";
             vm.Password = "test123!";
-			Assert.IsTrue(vm.IsFormValid);
+            Assert.IsTrue(vm.IsFormValid);
+            Assert.IsTrue(vm.LoginCommand.CanExecute(null));
         }
     }
 }
